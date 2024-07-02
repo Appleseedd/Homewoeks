ff8af43d02a50d137ea74e4c20f8d58668c2b5a6
Appleseedd <slender040@gmail.com>
2024-07-02 21:45:31 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,6 @@
+my_string = input('Перечислите Ваши любимые времена года: ')
+print(my_string.__len__()) # если я правильно понял который именно текст, если текст с первой строки, то я не поинмаю куда вставить команду __len__
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
