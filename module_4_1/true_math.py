b1eadbc8225718ed9a8f97d871366dfe20de5252
Appleseedd <slender040@gmail.com>
2024-08-11 17:25:05 +0300

    Add files via upload

---
+++ module_4_1/true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+def divide(first, second):
+    while True:
+        try:
+            first / second
+            if second > 0:
+                return first / second
+        except ZeroDivisionError:
+            return inf
